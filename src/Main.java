35d409fdcace864fd038331c2bbffcf4941cde1b
Nazarii Dunas <dunas.nazar@gmail.com>
2026-01-14 09:46:06 +0100

    Add initial project files

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+// Simple Hello World program
+public class Main {
+    public static void main(String[] args) {
+        // Print "Hello World" to standard output
+        System.out.println("Hello World");
+    }
+}
