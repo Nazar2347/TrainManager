5573e3bc4832d59f19e0824579b92d71646c1fb3
Nazarii Dunas <dunas.nazar@gmail.com>
2026-01-18 12:38:51 +0100

    Added basic logic for cart and wagon + children classes Added locomotive wagons management logic Added passenger and cargo variables and mass logic

--- src/GameObjects/Wagon.java
+++ src/GameObjects/Wagon.java
@@ -1,14 +1,18 @@
 package GameObjects;
+  
+import java.util.HashSet;
 
-public abstract class Wagon extends Cart {
-    protected float massCapacity;
-protected void LoadCargo()
-{
-    //TODO: Implementation for loading cargo}
-}
+public abstract class Wagon extends Cart
+ {
+    
+    protected HashSet<Cargo> loadedCargo;
+    public Wagon(float emptyMass, float maxMass)
+    {
+        super(emptyMass, maxMass);
+    };
+    
+    
+    protected abstract void LoadCargo(Cargo cargo);
 
-protected void UnloadCargo()
-{
-    //TODO: Implementation for unloading cargo}
-}
+    protected abstract void UnloadCargo();
 }
